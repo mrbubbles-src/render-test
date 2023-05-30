c66e251b1ff89e4bbbb7b59071611a7cb59c2cd2
mrbubbles-src <feaver5683@gmail.com>
2023-05-30 14:41:59 +0200

    testing the second

--- server.js
+++ server.js
@@ -10,9 +10,18 @@ const apiKey = config.apiKey;
 
 console.log("PORT", port);
 app.get("/", (req, res) => {
+    // let link = document.createElement("a");
+    // a.setAttribute(
+    // "href",
+    // "https://be-13-env-mrbubbles-src-render-test.onrender.com/halloIchBinEinApiKey"
+    // );
+    // a.innerHTML = "Click me";
+    // document.body.appendChild(a);
     res.send(
-        "You can finde the test site @ https://be-13-env-mrbubbles-src-render-test.onrender.com/halloIchBinEinApiKey"
+        `You can finde the test site <a href="https://be-13-env-mrbubbles-src-render-test.onrender.com/halloIchBinEinApiKey">Click Me</a>`
     );
+
+    // res.send(`You can finde the test site ${link}`);
 });
 app.get(`/${apiKey}`, (req, res) => {
     res.send("Hi Api Key");
